18d5d5b2d2c954491ba0a7c1985ac44717fe5426
Bhoomika.N <n.bhoomika565@gmail.com>
2025-05-30 11:54:55 +0530

    Initial commit

---
+++ java.js
@@ -0,0 +1,70 @@
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String publisher;
+    private int year;
+    private boolean isAvailable;
+
+    public Book(int id, String title, String author, String publisher, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+        this.isAvailable = true;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %d | Title: %s | Author: %s | Publisher: %s | Year: %d | Available: %s",
+                id, title, author, publisher, year, isAvailable ? "Yes" : "No");
+    }
+}
+
